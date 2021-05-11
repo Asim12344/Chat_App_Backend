c3bf4f15be62691041de2fdef8fae06f8469ee18
Asim12344 <m.asimwaqar18@gmail.com>
2021-05-11 07:28:10 +0500

    chat

--- chat.py
+++ chat.py
@@ -7,14 +7,42 @@ sio = socketio.AsyncServer(cors_allowed_origins='*', ping_timeout=35)
 app = web.Application()
 sio.attach(app)
 
+rooms = {}
 
 @sio.event
 async def connect(sid, environ):
     print('Connected', sid)
     await sio.emit('ready', room=ROOM, skip_sid=sid)
-    print("kjdkdjkdj")
     sio.enter_room(sid, ROOM)
 
+
+@sio.event
+async def join_room(sid, roomID):
+    print("roomID")
+    print(roomID)
+    print(type(roomID))
+    print("rooms")
+    print(rooms)
+    # print(rooms.)
+    if rooms:
+        rooms[roomID].append(sid)
+    else:
+        rooms[roomID] = [sid]
+    
+    print(rooms)
+    otherUser = None
+    # rooms[roomID].find(id => id !== socket.id)
+    for r in rooms[roomID]:
+        
+        if r != sid:
+            otherUser = r
+    print(otherUser)
+    if otherUser:
+        await sio.emit('other_user', otherUser , room=sid)
+        await sio.emit('user_joined', sid , room=otherUser)
+
+
+           
 
 @sio.event
 def disconnect(sid):
@@ -26,6 +54,27 @@ def disconnect(sid):
 async def data(sid, data):
     print('Message from {}: {}'.format(sid, data))
     await sio.emit('data', data, room=ROOM, skip_sid=sid)
+
+@sio.event
+async def offer(sid,payload):
+    # print('Message from {}: {}'.format(sid, data))
+    print("offer")
+    print(payload)
+    await sio.emit('offer', payload , room=payload['target'])
+
+@sio.event
+async def answer(sid,payload):
+    print("answer")
+    print(payload)
+    # print('Message from {}: {}'.format(sid, data))
+    await sio.emit('answer', payload, room=payload['target'])
+
+@sio.event
+async def ice_candidate(sid,payload):
+    # print('Message from {}: {}'.format(sid, data))
+    print("ice_candidate")
+    print(payload)
+    await sio.emit('ice_candidate', payload['candidate'], room=payload['target'])
 
 
 if __name__ == '__main__':
