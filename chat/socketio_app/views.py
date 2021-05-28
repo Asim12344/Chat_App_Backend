9ee0dd1116d4569fb330e56a097695e68375b0b1
Asim12344 <m.asimwaqar18@gmail.com>
2021-05-28 17:55:37 +0500

    chat

--- chat/socketio_app/views.py
+++ chat/socketio_app/views.py
@@ -7,7 +7,7 @@ import json
 from django.core.cache import cache
 from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
-
+import json
 
 
  
@@ -42,8 +42,7 @@ def connect(sid, environ):
 
 @sio.event
 def connected(sid, payload):
-    print("connected")
-    print(payload['token'])
+    print("payload = " ,payload['token'])
     token = 'Bearer ' + payload['token']
     try:
         response = requests.post(config._Auth_URL, auth=TokenAuth(token)).json()
@@ -57,42 +56,28 @@ def connected(sid, payload):
         print(status)
         if status == -1:
             print('Authenticated')
+            payload1 = {
+                "availabilityStatus": "Online",
+                "socketId":sid
+            }
+            response1 = requests.post("https://devplay.gamergraph.io/chat/player/availability/update/", data=json.dumps(payload1), headers={"Authorization": "Bearer " + payload['token'], "Content-Type": "application/json"})
+            print(response1.json())
+            obj = {"sid":sid, "email":payload['email'],"partner_sid":"","token":payload['token']}
+            cache.add(sid,obj,timeout=CACHE_TTL)
+            print("==============================")
+            print(cache.get(sid))
             sio.emit('token', "correct" , room=sid)
         else:
             print('Not Authenticated else')
-            sio.emit('token', "correct" , room=sid)
+            sio.emit('token', "incorrect" , room=sid)
     except:
         print('Not Authentication except')
     
-@sio.event
-def join_room(sid, roomID):
-    print("roomID")
-    print(roomID)
-    print(type(roomID))
-    print("rooms")
-    print(rooms)
-    # print(rooms.)
-    if rooms:
-        rooms[roomID].append(sid)
-    else:
-        rooms[roomID] = [sid]
-    
-    print(rooms)
-    otherUser = None
-    # rooms[roomID].find(id => id !== socket.id)
-    for r in rooms[roomID]:
-        if r != sid:
-            otherUser = r
-    print(otherUser)
-    if otherUser:
-        sio.emit('other_user', otherUser , room=sid)
-        sio.emit('user_joined', sid , room=otherUser)
-
 
 @sio.event
-def join_room2(sid,email):
+def join(sid,email):
     global users
-    print("join_room2")
+    print("join")
     print(email)
     if len(users) < 2:
         obj = {}
@@ -100,7 +85,6 @@ def join_room2(sid,email):
         users.append(obj)
         print(users)
         sio.emit('online_users', users)
-
     else:
         print("else")
         obj = {}
@@ -113,57 +97,40 @@ def join_room2(sid,email):
 def create_connection(sid,payload):
     print("create_connection")
     print(payload)
-    i=0
-    global users
-    for user in users:
-        if user['sid'] == payload[1]['sid']:
-            users[i]["partner_sid"]=payload[1]['target']
-        if user['sid'] == payload[0]['sid']:
-            users[i]["partner_sid"]=payload[0]['target']
-        i=i+1
-    
-    print(users)
-        
+    caller_sid = payload[0]['sid']
+    target_sid = payload[1]['sid']
+
+    get_caller_record = cache.get(caller_sid)
+    print("get_caller_record = " , get_caller_record)
+    get_caller_record['partner_sid'] = payload[1]['sid']
+    print("get_caller_record1 = " , get_caller_record)
+    cache.set(caller_sid,get_caller_record,timeout=CACHE_TTL)
+
+    get_target_record = cache.get(target_sid)
+    print("get_target_record = " , get_target_record)
+    get_target_record['partner_sid'] = payload[0]['sid']
+    print("get_target_record1 = " , get_target_record)
+    cache.set(target_sid,get_target_record,timeout=CACHE_TTL)
+
     sio.emit('other_user', payload[1] , room=payload[0]['sid'])
     sio.emit('user_joined', payload[0] , room=payload[1]['sid'])
-
-@sio.event
-def join_room1(sid):
-    print("join_room1")
-    if len(users) == 0:
-        users.append(sid)
-        print(users)
-        sio.emit('no_user', users , room=sid)
-    elif len(users) == 1:
-        users.append(sid)
-        print(users)
-        sio.emit('other_user', users[0] , room=sid)
-        sio.emit('user_joined', sid , room=users[0])
-    else:
-        users.append(sid)
-        print("else")
-        sio.emit('third_user' , room=sid)
 
 @sio.event
 def disconnect(sid):
     print(sid)
     print(type(sid))
-    global users
-    print(users)
-    i = 0
-    for user in users:
-        if user['sid'] == sid:
-            break
-        i=i+1
-    print(i)
-    if len(users) > 0:
-        temp = users[i]
-        print("temp = " , temp)
-        users.pop(i)
-        print(users)
-        sio.emit('disconnect', sid)
-        sio.emit('partner', room=temp['partner_sid'])
-   
+    removed_record = cache.get(sid)
+    print("======== disconnect ===========")
+    print(removed_record)
+    print(removed_record['token'])
+    cache.delete(sid)
+    payload1 = {
+        "availabilityStatus": "Offline",
+        "socketId":""
+    }
+    response1 = requests.post("https://devplay.gamergraph.io/chat/player/availability/update/", data=json.dumps(payload1), headers={"Authorization": "Bearer " + removed_record['token'], "Content-Type": "application/json"})
+    print(response1.json())
+    sio.emit('partner', room=removed_record['partner_sid'])
     sio.leave_room(sid, ROOM)
     print('Disconnected', sid)
     
@@ -171,11 +138,10 @@ def disconnect(sid):
 @sio.event
 def offer(sid,payload):
     print('Message from {}: {}'.format(sid, payload))
-    cache.set('sdp',payload,timeout=CACHE_TTL)
-
-    test = cache.get('sdp')
-    print("test")
-    print(test)
+    # cache.set('sdp',payload,timeout=CACHE_TTL)
+    # test = cache.get('sdp')
+    # print("test")
+    # print(test)
     print("offer")
     print(payload)
     sio.emit('offer', payload , room=payload['target'])
@@ -185,12 +151,10 @@ def offer(sid,payload):
 def answer(sid,payload):
     print("answer")
     print(payload)
-    # print('Message from {}: {}'.format(sid, data))
     sio.emit('answer', payload, room=payload['target'])
 
 @sio.event
 def ice_candidate(sid,payload):
-    # print('Message from {}: {}'.format(sid, data))
     print("ice_candidate")
     print(payload)
     sio.emit('ice_candidate', payload['candidate'], room=payload['target'])
